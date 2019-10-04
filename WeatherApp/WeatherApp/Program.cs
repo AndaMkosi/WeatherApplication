76f3d42d9ca71bf88315cd84a3b0d3cefc69f324
AndaMkosi <38075130+AndaMkosi@users.noreply.github.com>
2019-10-04 13:09:32 +0200

    Weather Appplication

---
+++ WeatherApp/WeatherApp/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Threading.Tasks;
+using WeatherAppLibrary;
+
+
+namespace WeatherAppLibrary
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            //Console.WriteLine("Hello World!");
+            WeatherGenerator weatherGenerator = new WeatherGenerator();
+            string cptWeather = await weatherGenerator.GetWeather();                      
+
+            Console.WriteLine(cptWeather);
+
+
+        }
+    }
+}
---
+++ WeatherApp/WeatherAppLibrary/WeatherGenerator.cs
@@ -0,0 +1,27 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WeatherAppLibrary
+{
+   public class WeatherGenerator
+    {
+        public async Task<string> GetWeather()  //Method
+        {
+            HttpClient client = new HttpClient();
+            String cptWeather = await client.GetStringAsync("http://api.openweathermap.org/data/2.5/weather?q=Cape%20Town&appid=248ca220af551521c2584c4f18912d69");
+            var weather = JsonConvert.DeserializeObject<WeatherData>(cptWeather);
+
+            string returnString = string.Empty;
+
+            foreach (var weatherItem in weather.weather)
+                returnString = weatherItem.description;
+
+            return returnString;
+
+        }
+    }
+}
